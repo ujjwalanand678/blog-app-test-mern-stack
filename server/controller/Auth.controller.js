28d3b0f8bad7b85c65ae0eacece013a7c9ab3497
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-09-17 20:24:23 +0530

    verify token

--- server/controller/Auth.controller.js
+++ server/controller/Auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../model/User.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
 
 export const userRegister = async (req, res, next) => {
   const { name, email, password, phone, profilePic } = req.body;
@@ -33,6 +34,12 @@ export const userRegister = async (req, res, next) => {
   }
 };
 
+const createJwtToken = (user)=>{
+return jwt.sign({id : user._id , role: user.role, name: user.name}, process.env.JWT_TOKEN_KEY,{expiresIn: "1d"}) 
+// it will create a token with the user id and role and name as payload and sign it with the secret key and set the expiry time to 1 day.
+// we can store any other information in the payload as well.
+}
+
 export const userLogin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
@@ -51,10 +58,14 @@ export const userLogin = async (req, res, next) => {
         .status(400)
         .json({ success: false, message: "Invalid password" });
     }
+    const token = createJwtToken(user)
+    const { password: hashedPassword, role, ...rest}= user._doc; 
+    // we dont want to send password and role in response. so we are using destructuring to exclude them. {...rest} will contain all the other fields except password and role.
     return res
       .status(200)
-      .json({ success: true, message: "User logged in successfully" });
+      .json({ success: true, message: "User logged in successfully" , token , role, data: rest});
   } catch (error) {
+     console.error("Login Error:", error);
     return res.status(500).json({ success: false, message: "Server error" });
   }
 };
--- server/routes/Blog.routes.js
+++ server/routes/Blog.routes.js
@@ -1,19 +1,20 @@
 import express from "express"; // import express framework
 import { createBlog, deleteBlog, editBlog, getAllBlogs, getBlogByTopic, getBlogsByMultipleTopics, getBlogsByQuery, getSingleBlog } from "../controller/Blog.controller.js";
+import { authorize } from "../auth/VerifyToken.js";
 
 const route = express.Router(); // The line in a Node.js Express app creates a new router instance using Express’s Router class. This router object can be used to define modular, mountable route handlers.
 
 // http://localhost:8080/api/v1/blog/createblog
-route.post("/createblog", createBlog); // This line defines a POST route at the path "/createblog" using the router instance. When a POST request is made to this path, the createBlog controller function will be executed to handle the request.
+route.post("/createblog",authorize, createBlog); // This line defines a POST route at the path "/createblog" using the router instance. When a POST request is made to this path, the createBlog controller function will be executed to handle the request.
 
 // http://localhost:8080/api/v1/blog/editblog/68b08eef047aa5ba210fe62c
-route.put("/editblog/:id" , editBlog)
+route.put("/editblog/:id" ,authorize, editBlog)
 
 //http://localhost:8080/api/v1/blog/getallblogs
 route.get("/getallblogs", getAllBlogs)
 
 //http://localhost:8080/api/v1/blog/deteleblog/id
-route.delete("/deteleblog/:id",deleteBlog)
+route.delete("/deteleblog/:id",authorize,deleteBlog)
 export default route; // export the router instance for use in other parts of the application
 
 //http://localhost:8080/api/v1/blog/getsingleblog/id
